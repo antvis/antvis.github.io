bde4f38ad858a7a986d5ec991962570f903ffcd1
afc163 <afc163@gmail.com>
2019-09-17 17:27:36 +0800

    `gatsby-plugin-offline`

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
