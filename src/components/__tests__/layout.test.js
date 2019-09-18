3f04e2fb2b61d4175713eac2b9e084ca426cd0fd
afc163 <afc163@gmail.com>
2019-09-18 12:02:42 +0800

    :white_check_mark: Add jest and ui test

---
+++ src/components/__tests__/layout.test.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { useStaticQuery } from 'gatsby' // mocked
+
+import Layout from '../layout'
+
+beforeEach(() => {
+  useStaticQuery.mockImplementationOnce(() => ({
+    site: {
+      siteMetadata: {
+        title: `GatsbyJS`,
+      },
+    },
+  }))
+})
+
+describe(`Layout`, () => {
+  it(`renders a header`, () => {
+    const { container } = render(
+      <Layout>
+        <main>
+          <h1>hello</h1>
+        </main>
+      </Layout>
+    )
+
+    expect(container.querySelector(`header`)).toBeInTheDocument()
+  })
+
+  it(`renders children`, () => {
+    const text = `__Hello world__`
+    const { getByText } = render(
+      <Layout>
+        <main>
+          <h1>{text}</h1>
+        </main>
+      </Layout>
+    )
+
+    const child = getByText(text)
+
+    expect(child).toBeInTheDocument()
+  })
+})
