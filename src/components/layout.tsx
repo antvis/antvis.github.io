e0216896fcd3015ebced204bf436ad344584303a
afc163 <afc163@gmail.com>
2019-09-19 10:47:12 +0800

    :sparkles: footer data

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -27,29 +27,156 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     }
   `);
 
+  const footerColumns = [{
+    title: 'G2',
+    items: [
+      {
+        title: '关于 G2',
+        url: '',
+      },
+      {
+        title: '图表示例',
+        url: '',
+      },
+      {
+        title: 'API 文档',
+        url: '',
+      },
+      {
+        title: '使用教程',
+        url: '',
+      },
+    ],
+  }, {
+    title: 'G6',
+    items: [
+      {
+        title: '关于 G2',
+        url: '',
+      },
+      {
+        title: '图表示例',
+        url: '',
+      },
+      {
+        title: 'API 文档',
+        url: '',
+      },
+      {
+        title: '使用教程',
+        url: '',
+      },
+    ],
+  }, {
+    title: 'F2',
+    items: [
+      {
+        title: '关于 G2',
+        url: '',
+      },
+      {
+        title: '图表示例',
+        url: '',
+      },
+      {
+        title: 'API 文档',
+        url: '',
+      },
+      {
+        title: '使用教程',
+        url: '',
+      },
+    ],
+  }, {
+    title: 'L7',
+    items: [
+      {
+        title: '关于 G2',
+        url: '',
+      },
+      {
+        title: '图表示例',
+        url: '',
+      },
+      {
+        title: 'API 文档',
+        url: '',
+      },
+      {
+        title: '使用教程',
+        url: '',
+      },
+    ],
+  }, {
+    title: '墨者学院',
+    items: [
+      {
+        title: '关于 G2',
+        url: '',
+      },
+      {
+        title: '图表示例',
+        url: '',
+      },
+      {
+        title: 'API 文档',
+        url: '',
+      },
+      {
+        title: '使用教程',
+        url: '',
+      },
+    ],
+  }, {
+    icon: (
+      <img
+        src="https://gw.alipayobjects.com/zos/rmsportal/nBVXkrFdWHxbZlmMbsaH.svg"
+        alt="more products"
+      />
+    ),
+    title: '更多产品',
+    items: [
+      {
+        icon: (
+          <img
+            src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg"
+            alt="yuque"
+          />
+        ),
+        title: '语雀',
+        url: 'https://yuque.com',
+        description: '知识创作与分享工具',
+        openExternal: true,
+      },
+      {
+        icon: (
+          <img
+            src="https://gw.alipayobjects.com/zos/rmsportal/uHocHZfNWZOdsRUonZNr.png"
+            alt="yuque"
+          />
+        ),
+        title: '云凤蝶',
+        url: 'https://yunfengdie.com',
+        description: '中台建站平台',
+        openExternal: true,
+      },
+    ],
+  }]
+
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <Footer
-          columns={[]}
-          bottom={
-            <div>
-              © {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </div>
-          }
-        />
-      </div>
+      <main>{children}</main>
+      <Footer
+        columns={footerColumns}
+        bottom={
+          <div>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="http://xtech.antfin.com/">AFX</a>
+          </div>
+        }
+      />
     </>
   );
 };
