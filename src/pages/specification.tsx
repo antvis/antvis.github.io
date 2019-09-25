2320124aedd3a3136de9bea87b7e68de7f53215e
afc163 <afc163@gmail.com>
2019-09-25 18:39:05 +0800

    article content

--- src/pages/specification.tsx
+++ src/pages/specification.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-
+import { Layout as AntLayout, Menu } from 'antd';
 import Layout from '../components/layout';
+import Article from '../components/article';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>设计文档</h1>
+    <AntLayout style={{ background: '#fff' }}>
+      <AntLayout.Sider width={240} theme="light">
+        <Menu mode="inline" defaultSelectedKeys={['home']} style={{ height: '100%' }}>
+          <Menu.Item key="home">首页</Menu.Item>
+          <Menu.Item>文章</Menu.Item>
+          <Menu.Item>文章</Menu.Item>
+          <Menu.Item>文章</Menu.Item>
+          <Menu.Item>文章</Menu.Item>
+          <Menu.Item>文章</Menu.Item>
+          <Menu.Item>文章</Menu.Item>
+        </Menu>
+      </AntLayout.Sider>
+      <Article>
+        <h2>标题</h2>
+        内容
+      </Article>
+    </AntLayout>
   </Layout>
 );
 
