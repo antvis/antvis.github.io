459cef7768bd9b5d19d9386d723bc4c8409153a3
afc163 <afc163@gmail.com>
2019-09-25 19:53:00 +0800

    fix active menu and order

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,7 +13,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   const result = await graphql(`
     {
       allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
+        sort: { order: ASC, fields: [order] }
         limit: 1000
       ) {
         edges {
