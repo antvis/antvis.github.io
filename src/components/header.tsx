3a9e962519d7a797c8ba0a782a9c96f5776978ef
afc163 <afc163@gmail.com>
2019-09-25 15:57:42 +0800

    :zap: Header!

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,35 +1,38 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import GithubCorner from 'react-github-corner';
+import Search from './search';
+import styles from './header.module.less';
 
 interface HeaderProps {
   siteTitle?: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className={styles.header}>
+    <div className={styles.left}>
+      <Link to="/">
+        <img src="https://gw.alipayobjects.com/os/s/prod/antv/assets/image/logo-with-text-73b8a.svg" alt={siteTitle} />
+      </Link>
+      <span className={styles.divider} />
+      <Search />
     </div>
+    <nav className={styles.nav}>
+      <ul className={styles.menu}>
+        <li>
+          <Link to="/specification">
+            设计语言
+          </Link>
+        </li>
+        <li>
+          所有产品
+        </li>
+        <li>
+          生态
+        </li>
+      </ul>
+      <GithubCorner href="https://github.com/antvis" size={64} />
+    </nav>
   </header>
 );
 
