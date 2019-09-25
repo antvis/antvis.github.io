26f7c8e1d9085775df51a1f337b8c1b69cce54cf
afc163 <afc163@gmail.com>
2019-09-25 16:12:24 +0800

    高亮样式

---
+++ src/pages/specification.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>设计文档</h1>
+  </Layout>
+);
+
+export default IndexPage;
