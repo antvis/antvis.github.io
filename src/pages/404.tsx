ee7d0ff91dadd9ea1cf6be64478fbe739af5b17f
afc163 <afc163@gmail.com>
2019-09-27 17:51:59 +0800

    update home and 404 pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-
+import { Result, Button, Icon } from 'antd';
+import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Link to="/">
+          <Button type="primary">
+            <Icon type="home" />
+            Back Home
+          </Button>
+        </Link>
+      }
+    />
   </Layout>
 );
 
