d660f313df1854b51e377fd94c4cd113896bf230
afc163 <afc163@gmail.com>
2019-09-29 20:26:42 +0800

    import react i18next

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from './search.module.less';
 
-export default () => <input className={styles.input} placeholder="Search..." />;
+export default () => {
+  const { t } = useTranslation();
+  return (
+    <input className={styles.input} placeholder={t('搜索…')} />
+  );
+};
