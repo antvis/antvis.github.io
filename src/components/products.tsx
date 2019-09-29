4de90fc037d381786f1d09f9abb6b67f95254c67
afc163 <afc163@gmail.com>
2019-09-29 19:47:23 +0800

    split products and product components

---
+++ src/components/products.tsx
@@ -0,0 +1,188 @@
+import React from 'react';
+import Product from './product';
+import styles from './product.module.less';
+
+const Products: React.FC<{}> = () => (
+  <div className={styles.products}>
+    <h3>基础产品</h3>
+    <ul>
+      <Product
+        name="G2"
+        url="/g2"
+        description="一套基于可视化编码的图形语法"
+        icon="https://gw.alipayobjects.com/zos/antfincdn/mpz%24sFOwuD/1852987f-0412-44e5-9d0d-75b06f392778.png"
+        links={[
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+        ]}
+      />
+      <Product
+        name="G2"
+        description="一套基于可视化编码的图形语法"
+        icon="https://gw.alipayobjects.com/zos/antfincdn/mpz%24sFOwuD/1852987f-0412-44e5-9d0d-75b06f392778.png"
+        links={[
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+        ]}
+      />
+      <Product
+        name="G2"
+        description="一套基于可视化编码的图形语法"
+        icon="https://gw.alipayobjects.com/zos/antfincdn/mpz%24sFOwuD/1852987f-0412-44e5-9d0d-75b06f392778.png"
+        links={[
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+        ]}
+      />
+      <Product
+        name="G2"
+        description="一套基于可视化编码的图形语法"
+        icon="https://gw.alipayobjects.com/zos/antfincdn/mpz%24sFOwuD/1852987f-0412-44e5-9d0d-75b06f392778.png"
+        links={[
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+        ]}
+      />
+      <Product
+        name="G2"
+        description="一套基于可视化编码的图形语法"
+        icon="https://gw.alipayobjects.com/zos/antfincdn/mpz%24sFOwuD/1852987f-0412-44e5-9d0d-75b06f392778.png"
+        links={[
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+        ]}
+      />
+      <Product
+        name="G2"
+        description="一套基于可视化编码的图形语法"
+        icon="https://gw.alipayobjects.com/zos/antfincdn/mpz%24sFOwuD/1852987f-0412-44e5-9d0d-75b06f392778.png"
+        links={[
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+        ]}
+      />
+      <Product
+        name="G2"
+        description="一套基于可视化编码的图形语法"
+        icon="https://gw.alipayobjects.com/zos/antfincdn/mpz%24sFOwuD/1852987f-0412-44e5-9d0d-75b06f392778.png"
+        links={[
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+        ]}
+      />
+    </ul>
+    <h3>扩展产品</h3>
+    <ul>
+      <Product
+        name="G2"
+        description="一套基于可视化编码的图形语法"
+        icon="https://gw.alipayobjects.com/zos/antfincdn/mpz%24sFOwuD/1852987f-0412-44e5-9d0d-75b06f392778.png"
+        links={[
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+        ]}
+      />
+      <Product
+        name="G2"
+        description="一套基于可视化编码的图形语法"
+        icon="https://gw.alipayobjects.com/zos/antfincdn/mpz%24sFOwuD/1852987f-0412-44e5-9d0d-75b06f392778.png"
+        links={[
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+          {
+            name: '关于',
+            link: '',
+          },
+        ]}
+      />
+    </ul>
+  </div>
+);
+
+export default Products;
