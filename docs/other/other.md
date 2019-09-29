246e9e9c3b12e6da94f2538ed7feeac99cbafe26
afc163 <afc163@gmail.com>
2019-09-29 16:20:36 +0800

    :globe_with_meridians: i18n docs support

--- docs/other/other.md
+++
@@ -1,6 +0,0 @@
----
-title: Other
-order: 0
----
-
-测试用。
---
+++ src/pages/index.en.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Skeleton } from 'antd';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <div style={{ margin: '0 auto', padding: '0 80px' }}>
+      Home Page
+      <Skeleton />
+      <Skeleton />
+      <Skeleton />
+      <Skeleton />
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
