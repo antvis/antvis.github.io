eed1f67979633de64e1b6c0c1496511f4690bc89
afc163 <afc163@gmail.com>
2019-09-30 12:51:18 +0800

    fix react-helmet ts error

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
 interface SEOProps {
