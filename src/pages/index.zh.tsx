cf1b4b3b4bf3429019609299470f12e685dbc305
afc163 <afc163@gmail.com>
2019-10-09 18:42:58 +0800

    update site title

--- src/pages/index.zh.tsx
+++ src/pages/index.zh.tsx
@@ -5,7 +5,7 @@ import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="首页" lang="zh" />
+    <SEO title="蚂蚁数据可视化" lang="zh" />
     <div style={{ margin: '0 auto', padding: '0 80px' }}>
       中文首页
       <Skeleton />
