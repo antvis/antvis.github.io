b6af47039b4b76aa4a76cda131b7e9d2c3bd7107
afc163 <afc163@gmail.com>
2019-10-12 20:23:38 +0800

    upgrade theme to 0.2.0

--- site/pages/index.en.tsx
+++ site/pages/index.en.tsx
@@ -1,19 +1,3 @@
-import React from 'react';
-import { Skeleton } from 'antd';
-import Layout from '@antv/gatsby-theme-antv/site/components/layout';
-import SEO from '@antv/gatsby-theme-antv/site/components/seo';
+import Index from './index.zh';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" lang="en" />
-    <div style={{ margin: '0 auto', padding: '0 80px' }}>
-      Home Page
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-    </div>
-  </Layout>
-);
-
-export default IndexPage;
+export default Index;
--- site/pages/index.zh.tsx
+++ site/pages/index.zh.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Skeleton } from 'antd';
-import Layout from '@antv/gatsby-theme-antv/site/components/layout';
+import { useTranslation } from 'react-i18next';
 import SEO from '@antv/gatsby-theme-antv/site/components/seo';
 import astronaut from '../images/gatsby-astronaut.png';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="蚂蚁数据可视化" lang="zh" />
-    <div style={{ margin: '0 auto', padding: '0 80px' }}>
-      中文首页
-      <img src={astronaut} alt="astronaut" width="200" />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-    </div>
-  </Layout>
-);
+const IndexPage = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <SEO title="蚂蚁数据可视化" lang="zh" />
+      <div style={{ margin: '0 auto', padding: '0 80px' }}>
+        {t('首页')}
+        <img src={astronaut} alt="astronaut" height="200" />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </div>
+    </>
+  );
+};
 
 export default IndexPage;
