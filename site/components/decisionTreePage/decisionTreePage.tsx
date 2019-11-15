c401b154023cb721c54e813c09e299495329468c
Yanyan-Wang <yanyanwang93@gmail.com>
2019-11-15 19:21:19 +0800

    delete: unnecessary import.

--- site/components/decisionTreePage/decisionTreePage.tsx
+++ site/components/decisionTreePage/decisionTreePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useLayoutEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
 import styles from './decisionTreePage.module.less';
