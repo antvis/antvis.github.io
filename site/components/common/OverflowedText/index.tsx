4d35e1838e298d8d64577b9974414b3f90213981
Aaron <yangtao.yangtao@antgroup.com>
2025-01-08 11:24:58 +0800

    fix: remove PropTypes

--- site/components/common/OverflowedText/index.tsx
+++ site/components/common/OverflowedText/index.tsx
@@ -1,5 +1,4 @@
 import cx from 'classnames';
-import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 import { Tooltip, ConfigProvider } from 'antd';
 import { isNumber, isString, toNumber, omit } from 'lodash';
@@ -12,32 +11,26 @@ type Props = {
   maxWidth: string | number;
   maxHeight?: string | number;
   style?: Record<string, any>;
-}
+};
 
 const getPixel = (value: string | number) => {
   if (isNumber(value)) return `${value}px`;
   return value;
-}
+};
 
 const getNumber = (value: string | number) => {
   if (isString(value)) return toNumber(value.replace(/[^0-9]+/, ''));
   return value;
-}
+};
 
 // 自动省略并提示组件
 export class OverflowedText extends PureComponent<Props> {
-  static propTypes = {
-    className: PropTypes.string,
-    text: PropTypes.string,
-    maxWidth: PropTypes.string,
-  };
-
   static defaultProps = {
     className: '',
     text: 'overflowed text',
     maxWidth: '100px',
     style: {},
-  }
+  };
 
   state = {
     isOverflow: false,
@@ -60,16 +53,34 @@ export class OverflowedText extends PureComponent<Props> {
     this.$text = node;
     if (!node) return;
     const { maxHeight } = this.props;
-    this.setState({ isOverflow: maxHeight ? node.scrollHeight > node.offsetHeight : node.scrollWidth > node.offsetWidth });
+    this.setState({
+      isOverflow: maxHeight
+        ? node.scrollHeight > node.offsetHeight
+        : node.scrollWidth > node.offsetWidth,
+    });
   };
 
   render() {
     const { text, className, maxWidth, maxHeight, style = {} } = this.props;
     const { fontSize, lineHeight } = style;
-    const props = omit(this.props, ['className', 'text', 'maxWidth', 'maxHeight', 'style', 'width']);
-    const cls = cx('component-overflowed-text', className, maxHeight && 'max-height-text');
+    const props = omit(this.props, [
+      'className',
+      'text',
+      'maxWidth',
+      'maxHeight',
+      'style',
+      'width',
+    ]);
+    const cls = cx(
+      'component-overflowed-text',
+      className,
+      maxHeight && 'max-height-text',
+    );
 
-    let textStyle: Record<string, string> = { maxWidth: getPixel(maxWidth), ...style };
+    let textStyle: Record<string, string> = {
+      maxWidth: getPixel(maxWidth),
+      ...style,
+    };
     // 最大高度 转化为 行数， 需要  fontSize, lineHeight 属性
     if (maxHeight && fontSize && lineHeight) {
       const lineHeightSize = getNumber(fontSize) * lineHeight;
@@ -78,20 +89,29 @@ export class OverflowedText extends PureComponent<Props> {
         maxWidth: 'none',
         display: '-webkit-box',
         '-webkitBoxOrient': 'vertical',
-        '-webkitLineClamp': (Math.max(getNumber(maxHeight), lineHeightSize) / lineHeightSize).toFixed(0),
+        '-webkitLineClamp': (
+          Math.max(getNumber(maxHeight), lineHeightSize) / lineHeightSize
+        ).toFixed(0),
       };
     }
 
-    return <div className={cls} style={textStyle} ref={this.init}>
-      {this.state.isOverflow ? (
-        <ConfigProvider prefixCls="antd5">
-          <Tooltip title={text} placement="topLeft" overlayStyle={{ maxWidth: '50vw' }} {...props}>
-            {text}
-          </Tooltip>
-        </ConfigProvider>
-      ) : (
-        text
-      )}
-    </div>
+    return (
+      <div className={cls} style={textStyle} ref={this.init}>
+        {this.state.isOverflow ? (
+          <ConfigProvider prefixCls="antd5">
+            <Tooltip
+              title={text}
+              placement="topLeft"
+              overlayStyle={{ maxWidth: '50vw' }}
+              {...props}
+            >
+              {text}
+            </Tooltip>
+          </ConfigProvider>
+        ) : (
+          text
+        )}
+      </div>
+    );
   }
 }
