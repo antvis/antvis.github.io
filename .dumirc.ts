ac8d8796ab08b48c7c4e05c8a0e7e0d1b0e0b60a
Aaron <yangtao.yangtao@antgroup.com>
2025-01-08 14:44:15 +0800

    fix: fix route

--- .dumirc.ts
+++ .dumirc.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     },
     navs: [
       {
-        slug: 'docs/specification',
+        slug: 'docs/specification/principles/basic',
         title: {
           zh: '设计语言',
           en: 'Design',
